91faac732cff9a1e643fc69c5fe8991efce314c4
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2022-05-24 19:56:13 +0200

    Add user inputs

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
-const app = Vue.createApp({
-  template: '<h1>Hello World</h1>'
+const change = Vue.createApp({
+  template: '<h1>Hello World </h1>'
 })
 
-app.mount('#app')
+
+const app = Vue.createApp({
+  // template: '<h1>Hello {{firstName}} </h1>',
+  data() {
+    return {
+      firstName: 'John',
+      lastName: 'Mafuta',
+      email: 'john@gmail.com',
+      gender: 'male',
+      picture: 'https://randomuser.me/api/portraits/men/10.jpg'
+
+    }
+  },
+  methods: {
+    getUser() {
+      console.log('user logged in ')
+      // template: '<h1>Hello World</h1>'
+    }
+  }
+})
+
+let x= 1
+
+if (x < 2) {
+  app.mount('#app')
+}
+else {
+  change.mount('#app')
+}
+
