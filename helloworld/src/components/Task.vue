5d5dd86bf4e65fc4a1119e8cf1767230286d67ac
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2022-05-25 12:28:43 +0200

    Add changes

--- helloworld/src/components/Task.vue
+++ helloworld/src/components/Task.vue
@@ -1,6 +1,10 @@
 <template>
-  <h3> {{ task.text }}</h3>
-  <p>{{ task.day }} </p>
+  <div :class='[task.reminder]'>
+    <h3> {{ task.text }} 
+      <i class="fas fa-times"></i>
+    </h3>
+    <p>{{ task.day }} </p>
+  </div>
 </template>
 
 <script>
@@ -8,6 +12,13 @@ export default {
   name: 'Task',
   props: {
     task: Object
-  }
+  },
 }
 </script>
+
+<style scoped>
+.fas {
+  color: red
+}
+
+</style>
