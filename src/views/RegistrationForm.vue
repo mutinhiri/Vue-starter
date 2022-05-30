3766e16006bc03b6db373aa58c54aca21dc4cc7c
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2022-05-30 19:16:14 +0200

    Add models

--- src/views/RegistrationForm.vue
+++ src/views/RegistrationForm.vue
@@ -7,7 +7,7 @@
               <label for="email"><b>Username</b></label>
             <input type="text" name="username" placeholder="Enter Username" required>
           <label for="email"><b>Email</b></label>
-          <input type="text" placeholder="Enter Email" name="email" required>
+          <input type="email" placeholder="Enter Email" name="email" v-model="email" required>
           
           <label for="psw"><b>Password</b></label>
           <input type="password" placeholder="Enter Password" name="psw" required>
