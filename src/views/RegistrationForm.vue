800fe0b96c15238bc4a3c3e955ab2fbf985bfe30
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2022-05-31 12:22:28 +0200

    Add styling for page

--- src/views/RegistrationForm.vue
+++ src/views/RegistrationForm.vue
@@ -4,20 +4,21 @@
           <h1>Sign Up</h1>
           <p>Please fill in this form to create an account.</p>
       
-              <label for="email"><b>Username</b></label>
-            <input type="text" name="username" placeholder="Enter Username" required>
+          <label for="username"><b>Username</b></label>
+          <input type="text" name="username" placeholder="Enter Username" v-model="username" required>
+
           <label for="email"><b>Email</b></label>
-          <input type="text" placeholder="Enter Email" name="email" required>
+          <input type="text" placeholder="Enter Email" name="email" v-model="email" required>
           
           <label for="psw"><b>Password</b></label>
-          <input type="password" placeholder="Enter Password" name="psw" required>
+          <input type="password" placeholder="Enter Password" name="psw" v-model="password" required>
       
           <p>By creating an account you agree to our <a href="#" style="color:dodgerblue">Terms & Privacy</a>.</p>
       
           <div class="clearfix">
-      
             <button type="submit" class="btn">Sign Up</button>
           </div>
+          
         </div>
       </form>
   <router-link to="/"><button>Log in </button> </router-link>
