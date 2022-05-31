d12cec270b66f16ab38a96ced43de75855354467
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2022-05-31 13:05:47 +0200

    Add changes after authentication

--- src/views/RegistrationForm.vue
+++ src/views/RegistrationForm.vue
@@ -1,5 +1,5 @@
 <template>
-    <form @submit="register">
+    <form @submit.prevent="register">
         <div class="container">
           <h1>Sign Up</h1>
           <p>Please fill in this form to create an account.</p>
@@ -16,7 +16,7 @@
           <p>By creating an account you agree to our <a href="#" style="color:dodgerblue">Terms & Privacy</a>.</p>
       
           <div class="clearfix">
-            <button type="submit" class="btn">Sign Up</button>
+            <button class="btn">Sign Up</button>
           </div>
 
         </div>
