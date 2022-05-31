9a016b99399c5ef55e1dab612c788682012807ed
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2022-05-31 14:01:47 +0200

    Add build

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import Amplify from 'aws-amplify';
+import Amplify, { Auth } from 'aws-amplify';
 import awsconfig from './aws-exports'; 
 Amplify.configure(awsconfig);
+Auth.configure(awsconfig)
 
 createApp(App).use(router).mount('#app')
